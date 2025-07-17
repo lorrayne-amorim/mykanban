632adf91cb1d5692ce3f050bf4e7bfd0ca75c8a7
lorrayne-amorim <lorrayne_afdc@hotmail.com>
2025-07-17 17:07:17 -0300

    fix: importing toaster and adding

--- mykanban/src/main.tsx
+++ mykanban/src/main.tsx
@@ -1,10 +1,12 @@
 import './index.css'
+import { Toaster } from "react-hot-toast";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster position="top-right" />
     <App />
   </StrictMode>,
 )
