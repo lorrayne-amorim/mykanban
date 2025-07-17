892da71e2ff13e875aab9e156101cedea22d9e75
lorrayne-amorim <lorrayne_afdc@hotmail.com>
2025-07-17 17:06:55 -0300

    fix: adjusting routes and adding page layout

--- mykanban/src/App.tsx
+++ mykanban/src/App.tsx
@@ -1,11 +1,19 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { BoardPage } from "./pages/BoardPage";
+import PageLayout from "./layouts/PageLayout";
 
 function App() {
   return (
-    <>
-
-    </>
-  )
+    <BrowserRouter>
+      <PageLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/board/:id" element={<BoardPage />} />
+        </Routes>
+      </PageLayout>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
